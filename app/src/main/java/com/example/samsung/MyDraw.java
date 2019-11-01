4221d3317b7bfd59b22dfecbfecc666614006747
Maxim <maxim_zykov@inbox.ru>
2019-11-01 11:04:22 +0300

    1.7.7 drawdiagonalline

--- app/src/main/java/com/example/samsung/MyDraw.java
+++ app/src/main/java/com/example/samsung/MyDraw.java
@@ -3,14 +3,34 @@ import android.content.Context;
 import android.graphics.*;
 import android.view.View;
 public class MyDraw extends View {
+    float[] points1;
     MyDraw(Context context) {
         super(context);
     }
     @Override
     protected void onDraw(Canvas canvas) {
         Paint paint = new Paint();
-        paint.setColor(Color.BLUE);
-        paint.setStyle(Paint.Style.FILL);
-        canvas.drawRect(100, 100, 300, 300, paint);
+        canvas.drawColor(Color.BLACK);
+        paint.setColor(Color.GREEN);
+        paint.setStrokeWidth(5);
+        float x = 0;float y =0;
+        while (x<getHeight()){
+            canvas.drawLine(x,0,0,y,paint);
+            x+=30;
+            y+=30;
         }
+        float z=0;
+        while (z < canvas.getHeight()){z +=1; }
+
+        while (x<z*2){
+            canvas.drawLine(x,0,0,y,paint);
+            x+=30;
+            y+=30;
+        }
+
+
+
     }
+    }
+
+
