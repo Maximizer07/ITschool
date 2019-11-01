860600154a2cd155d7d07c3e6e139fbbaf46af24
Maxim <maxim_zykov@inbox.ru>
2019-11-01 10:47:55 +0300

    1.7.6 drawRect

--- app/src/main/java/com/example/samsung/MyDraw.java
+++ app/src/main/java/com/example/samsung/MyDraw.java
@@ -2,24 +2,15 @@ package com.example.samsung;
 import android.content.Context;
 import android.graphics.*;
 import android.view.View;
-
 public class MyDraw extends View {
-
-    //конструктор – обсуждается в курсе позднее
     MyDraw(Context context) {
         super(context);
     }
-
     @Override
-
     protected void onDraw(Canvas canvas) {
         Paint paint = new Paint();
-        paint.setColor(Color.YELLOW);
+        paint.setColor(Color.BLUE);
         paint.setStyle(Paint.Style.FILL);
-        canvas.drawCircle(300, 300, 200, paint);
-        paint.setColor(Color.RED);
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(20);
-        canvas.drawCircle(300, 300, 200, paint);
+        canvas.drawRect(100, 100, 300, 300, paint);
         }
     }
